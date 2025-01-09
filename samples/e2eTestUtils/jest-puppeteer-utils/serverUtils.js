justjenb-fairoaksit/microsoft-authentication-library-for-js

commit 994cfde885ddfc49aba1378b2e57d95d6496398d

Addressed CodeQL Findings (#7486)
Addresses:

- Disabling certificate validation
- Information exposure through a stack trace
- Server-side URL redirect
    - Tested with redis

samples/e2eTestUtils/jest-puppeteer-utils/serverUtils.js
@@ -45,7 +45,6 @@ async function isServerUp(port, timeout) {
                 host: "localhost",
                 port: port,
                 family: 4,
-                rejectUnauthorized: false,
             };
 
             https

extensions/samples/msal-node-extensions/index.js
@@ -63,11 +63,9 @@ PersistenceCreator
         };
 
         pca.acquireTokenByCode(tokenRequest).then((response) => {
-            console.log("\nResponse: \n", response);
             res.sendStatus(200);
         }).catch((error) => {
-            console.error(error.errorMessage);
-            res.status(500).send(error);
+            res.status(500).send(error.errorMessage);
         });
     });
 

samples/msal-browser-samples/HybridSample/routes/auth.js
@@ -102,7 +102,7 @@ router.post('/server-redirect', (req, res) => {
         })
         .catch((error) => {
             console.timeEnd(timeLabel);
-            res.status(500).send(error);
+            res.status(500).send(error.errorMessage);
         });
 });
 

samples/msal-node-samples/auth-code-distributed-cache/src/middleware.ts
@@ -37,6 +37,8 @@ export const auth = (options: AuthOptions): Router => {
             );
         }
 
+        res.locals.originalUrl = `${req.protocol}://${req.get("host")}`;
+
         next();
     });
 
@@ -58,10 +60,7 @@ export const auth = (options: AuthOptions): Router => {
                 req.session.isAuthenticated = true;
                 req.session.account = tokenResponse?.account!; // account won't be null in this grant type
 
-                const { redirectTo } = JSON.parse(
-                    Buffer.from(req.body.state, "base64").toString("utf8")
-                );
-                res.redirect(redirectTo); // redirect back to original route
+                res.redirect(res.locals.originalUrl); // redirect back to original route
             } catch (error) {
                 next(error);
             }

samples/msal-node-samples/auth-code-key-vault/index.js
@@ -79,7 +79,7 @@ function msalApp(thumbprint, privateKey) {
         cca.acquireTokenByCode(tokenRequest).then((response) => {
             res.status(200).send('Congratulations! You have signed in successfully');
         }).catch((error) => {
-            res.status(500).send(error);
+            res.status(500).send(error.errorMessage);
         });
     });
 

samples/msal-node-samples/auth-code-pkce/src/index.ts
@@ -121,11 +121,11 @@ app.get("/redirect", (req: RequestWithPKCE, res) => {
     };
 
     pca.acquireTokenByCode(tokenRequest)
-        .then((response) => {
+        .then(() => {
             res.sendStatus(200);
         })
         .catch((error) => {
-            res.status(500).send(error);
+            res.status(500).send(error.errorMessage);
         });
 });
 

samples/msal-node-samples/auth-code-with-certs/index.js
@@ -88,7 +88,7 @@ app.post('/redirect', (req, res) => {
     cca.acquireTokenByCode(tokenRequest).then((response) => {
         res.status(200).send('Congratulations! You have signed in successfully');
     }).catch((error) => {
-        res.status(500).send(error);
+        res.status(500).send(error.errorMessage);
     });
 });
 

samples/msal-node-samples/auth-code/index.js
@@ -143,7 +143,7 @@ const getTokenAuthCode = function (scenarioConfig, clientApplication, port) {
             console.log("Successfully acquired token using Authorization Code.");
             res.sendStatus(200);
         }).catch((error) => {
-            res.status(500).send(error);
+            res.status(500).send(error.errorMessage);
         });
     });
 

commit cc2bdb2c7c9c3375a12a9f756cb363fa481bb271

Add back rejectUnauthorized request option to support self-signed certificates (#7499)
- Add back "rejectUnauthorized" request option to support self-signed
certificates
- Fixes regression in https samples introduced by changes in
https://github.com/AzureAD/microsoft-authentication-library-for-js/pull/7486

samples/e2eTestUtils/jest-puppeteer-utils/serverUtils.js
@@ -45,6 +45,7 @@ async function isServerUp(port, timeout) {
                 host: "localhost",
                 port: port,
                 family: 4,
+                rejectUnauthorized: false,
             };
 
             https